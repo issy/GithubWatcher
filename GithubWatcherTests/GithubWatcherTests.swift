715859d5aeb4005b277131867e76cabbae671911
issy <issy@issy.dev>
2026-06-09 21:58:03 +0100

    Initial commit

---
+++ GithubWatcherTests/GithubWatcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubWatcherTests.swift
+//  GithubWatcherTests
+//
+//  Created by Issy Szemeti on 03/06/2026.
+//
+
+import Testing
+@testable import GithubWatcher
+
+struct GithubWatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
