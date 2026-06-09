212acdef6358000c4dfaae0d067070814445a84b
issy <issy@issy.dev>
2026-06-09 23:01:18 +0100

    Whitespace

--- GithubWatcher/Features/WatchedRepositories/WatchedRepositoriesView.swift
+++ GithubWatcher/Features/WatchedRepositories/WatchedRepositoriesView.swift
@@ -3,13 +3,14 @@ import SwiftData
 
 struct WatchedRepositoriesView: View {
     @Environment(\.modelContext) private var modelContext
+
     @Query private var watchedRepositories: [WatchedRepository]
-    
+
     var body: some View {
         WatchedRepositoriesContentView(watchedRepositories: watchedRepositories.map{RepositoryItemData.fromModel($0)}, addWatchedRepository: {})
     }
 
     private func addWatchedRepository() {
-            modelContext.insert(WatchedRepository(owner: "issy", name: "app"))
+        modelContext.insert(WatchedRepository(owner: "issy", name: "app"))
     }
 }
